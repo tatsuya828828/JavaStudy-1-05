f6f239d7bb8bd1a4d9b3cbc8de39d8684524a1b8
tatsuya828828 <kkjy011894@gmail.com>
2020-07-09 23:59:39 +0900

    [finish]05-03

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
 
 public class Main {
-	public static void main(String[] args) {
-		/* フィールドを作成してもメソッドのブロック外のため使えない
-		String name = "湊";
-		int age = 20;
-		hello();
-		しかし、引数として渡せば使える
-		hello(name, age); */
-
-		hello("湊", 20);
-		// hello(20, "湊"); 引数の順番が違うとエラーになる
+	public static int add(int x, int y) {
+		int ans = x+y;
+		return ans;
 	}
 
-	// メソッド定義の際は他の人が分かりやすいような命名を心がける
-	public static void hello(String name, int age) {
-		System.out.println("名前:"+ name);
-		System.out.println("年齢:"+ age);
+	public static void main(String[] args) {
+		int ans = add(100,10);
+		System.out.println("100+10="+ ans);
+		System.out.println(add(add(10, 20), add(30,40)));
 	}
 }
