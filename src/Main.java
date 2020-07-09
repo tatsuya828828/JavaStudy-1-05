607edc0d0c29e18ec64ced4c4d8199927499a308
tatsuya828828 <kkjy011894@gmail.com>
2020-07-10 00:06:59 +0900

    [finish]05-04

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 
 public class Main {
+	public static int add(int x, int y, int z) {
+		return x+y+z;
+	}
 	public static int add(int x, int y) {
-		int ans = x+y;
-		return ans;
+		return x+y;
+	}
+
+	public static double add(double x, double y) {
+		return x+y;
+	}
+
+	public static String add(String x, String y) {
+		return x+y;
 	}
 
 	public static void main(String[] args) {
-		int ans = add(100,10);
-		System.out.println("100+10="+ ans);
-		System.out.println(add(add(10, 20), add(30,40)));
+		// 同じ名前のメソッドでも引数の型や数が違えば利用することができる
+		// 戻り値の方だけが異なるものは利用できない
+		// 呼び出す際は、引数でどのメソッドを呼び出すか判断される
+		System.out.println(add("Hello","World"));
+		System.out.println(add(10,50));
+		System.out.println(add(3.5,4.2));
+		System.out.println(add(4,5,6));
 	}
 }
