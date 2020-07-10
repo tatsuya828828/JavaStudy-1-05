1fb03b5bbf2c82c198ea321a3aac9b867f43a327
tatsuya828828 <kkjy011894@gmail.com>
2020-07-10 12:14:17 +0900

    [finish]05-05

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,45 @@
 
 public class Main {
-	public static int add(int x, int y, int z) {
-		return x+y+z;
-	}
-	public static int add(int x, int y) {
-		return x+y;
-	}
-
-	public static double add(double x, double y) {
-		return x+y;
+	public static void incArray(int[] array) {
+		for(int value: array) {
+			value++;
+			System.out.println(value);
+		}
 	}
 
-	public static String add(String x, String y) {
-		return x+y;
+	public static void inc(int[] array) {
+		for(int i=0; i<array.length; i++) {
+			array[i]++;
+			System.out.println(array[i]);
+		}
+	}
+
+	public static int[] makeArray(int size) {
+		int[] newArray = new int[size];
+		for(int i=0; i<newArray.length; i++) {
+			newArray[i] = i;
+		}
+		return  newArray;
 	}
 
 	public static void main(String[] args) {
-		// 同じ名前のメソッドでも引数の型や数が違えば利用することができる
-		// 戻り値の方だけが異なるものは利用できない
-		// 呼び出す際は、引数でどのメソッドを呼び出すか判断される
-		System.out.println(add("Hello","World"));
-		System.out.println(add(10,50));
-		System.out.println(add(3.5,4.2));
-		System.out.println(add(4,5,6));
+		int[] array = {1, 2, 3};
+		// 拡張for分の場合
+		incArray(array);
+		for(int value: array) {
+			System.out.println(value);
+		}
+		// for文の場合
+		// 引数にアドレスを渡すことによって呼び出し先で加えた変更が呼び出し元でも反映される
+		inc(array);
+		for(int value: array) {
+			System.out.println(value);
+		}
+
+		// 戻り値に配列を用いた場合
+		int[] array2 = makeArray(5);
+		for(int value: array2) {
+			System.out.println(value);
+		}
 	}
 }
